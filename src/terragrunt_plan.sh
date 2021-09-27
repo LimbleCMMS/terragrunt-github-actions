83e325de95bca7881f42eb8ea8a24c72a444b096
Jeff Babb <babb.jeff5@gmail.com>
2021-09-27 09:10:46 -0600

    update action

--- src/terragrunt_plan.sh
+++ src/terragrunt_plan.sh
@@ -2,6 +2,8 @@
 
 function terragruntPlan {
   # Gather the output of `terragrunt plan`.
+  which python3
+  
   echo "plan: info: planning Terragrunt configuration in ${tfWorkingDir}"
   planOutput=$(${tfBinary} plan -detailed-exitcode -input=false ${*} 2>&1)
   planExitCode=${?}
