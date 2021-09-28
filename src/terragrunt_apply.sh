69b09a9cec5e20c5f3b3ac5fce2f3eb836024442
Jeff Babb <babb.jeff5@gmail.com>
2021-09-28 10:34:09 -0600

    apply twice on exit 1

--- src/terragrunt_apply.sh
+++ src/terragrunt_apply.sh
@@ -7,14 +7,14 @@ function terragruntApply {
   applyExitCode=${?}
   applyCommentStatus="Failed"
 
-  if [ ${applyExitCode} -eq 2 ]; then
+  if [ ${applyExitCode} -eq 1 ]; then
     echo "apply: info: failed first run apply, trying again Terragrunt configuration in ${tfWorkingDir}"
     echo "${applyOutput}"
     applyOutput=$(${tfBinary} apply -auto-approve -input=false ${*} 2>&1)
     applyExitCode=${?}
     applyCommentStatus="Failed"
   fi
-  
+
   # Exit code of 0 indicates success. Print the output and exit.
   if [ ${applyExitCode} -eq 0 ]; then
     echo "apply: info: successfully applied Terragrunt configuration in ${tfWorkingDir}"
